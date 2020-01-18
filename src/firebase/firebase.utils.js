b04ff94d96bcb34a3eb5559d1e3c477118ba3534
Gabriela Olivares <gbs@MacBook-Pro-de-Gabriela.local>
2020-01-18 16:40:19 -0600

    Section 7: Sign up with user and email

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,30 @@ const config = {
     appId: "1:1008709511383:web:58d375e6af561a1a646892"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const userSnapshot = await userRef.get();
+    
+    if (!userSnapshot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch(e) {
+            console.log('Error creating user', e.message);
+        }
+    }
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
