5eb260883600c74b01bfe86a56abf5b480856f3f
Gabriela Olivares <gbs@MacBook-Pro-de-Gabriela.local>
2020-01-25 10:27:34 -0600

    Comments added to cart utils

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,6 +1,7 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const cartItemExists = cartItems.find(item => item.id === cartItemToAdd.id);
 
+    // When item exists, increase quantity
     if (cartItemExists) {
         return cartItems
             .map(cartItem =>
@@ -10,14 +11,17 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
             );
     }
 
+    // When item does NOT exists, create item with quantity equal to 1
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 }
 
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    // When it is the last item, remove from cart
     if (cartItemToRemove.quantity === 1) {
         return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
     }
 
+    // When it is NOT the last item, decrease the quantity's item
     return cartItems
         .map(cartItem => cartItem.id === cartItemToRemove.id ?
             { ...cartItem, quantity: cartItem.quantity - 1 } :
