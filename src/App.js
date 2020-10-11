fb5bbd4c6e25b4adc026b33abc5291856e597dba
Carlos Flores <gbs@MacBook-Pro-de-Gabriela.local>
2020-10-10 19:56:16 -0500

    Signin with Google saga implemented

--- src/App.js
+++ src/App.js
@@ -12,75 +12,40 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout.component';
 import ContactPage from './pages/contact/contact.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
-class App extends React.Component {
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    const { setCurrentUser } = this.props;
-
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      // Sets user either is authenticated or null
-      setCurrentUser(userAuth);
-
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
-
-        // With snapshot, the user is updated with the one saved in the database
-        // So if user is updated somewhere else in the app, onSnapshot listens
-        // to the changes and update current user data
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-            id: snapShot.id,
-            ...snapShot.data()
-          });
-        });
-      }
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route
-            exact
-            path='/'
-            render={() =>
-              this.props.currentUser ? (<HomePage />) : (<SignInAndSignUpPage />)
-            }
-          />
-          <Route 
-            path='/shop' 
-            render={({ match }) => this.props.currentUser ? (<ShopPage match={match} />) : (<Redirect to='/' />)} 
-          />
-          <Route path='/contact' component={ContactPage} />
-          <Route
-            path='/checkout' 
-            render={() =>
-              this.props.currentUser ? (<CheckoutPage />) : (<Redirect to='/' />)
-            }
-          />
-        </Switch>
-      </div>
-    );
-  }
+const App = (props) => {
+  const { currentUser } = props;
+  
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route
+          exact
+          path='/'
+          render={() =>
+            currentUser ? (<HomePage />) : (<SignInAndSignUpPage />)
+          }
+        />
+        <Route
+          path='/shop'
+          render={({ match }) => currentUser ? (<ShopPage match={match} />) : (<Redirect to='/' />)}
+        />
+        <Route path='/contact' component={ContactPage} />
+        <Route
+          path='/checkout'
+          render={() =>
+            currentUser ? (<CheckoutPage />) : (<Redirect to='/' />)
+          }
+        />
+      </Switch>
+    </div>
+  );
 }
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -27,17 +27,3 @@ export const fetchCollectionsStartAsync = () => dispatch => {
         dispatch(fetchCollectionsSuccess(e.message));
     });
 }
-
-// export const fetchCollectionsStartAsync = () => {
-//     return dispatch => {
-//         const collectionRef = firestore.collection('collections');
-//         dispatch(fetchCollectionsStart())
-
-//         collectionRef.get().then(snapshot => {
-//             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-//             dispatch(fetchCollectionsSuccess(collectionsMap));
-//         }).catch(e => {
-//             dispatch(fetchCollectionsSuccess(e.message));
-//         });
-//     };
-// }
